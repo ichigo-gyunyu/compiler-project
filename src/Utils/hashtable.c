db4ab0bd64948261a854ef0029144f67047564f9
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-02-26 19:25:22 +0530

    hashtable

--- src/Utils/hashtable.c
+++ src/Utils/hashtable.c
@@ -1,6 +1,12 @@
 #include "hashtable.h"
 
 // Computes the hash value based on d.key
-uint ht_hash(data d) {
+uint ht_hash(data d) {}
 
-}
+int ht_init(uint sz) {}
+
+void ht_print(hashtable ht) {}
+
+int ht_insert(hashtable ht, data d) {}
+
+int ht_lookup(hashtable ht, data d) {}
