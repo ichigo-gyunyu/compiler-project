8d781aa11d99edd8e3575577fa1bc43873b08f36
Sanjeet Kapadia <kapadia.sanjeet@gmail.com>
2022-02-28 20:55:35 +0530

    nary inorder

--- src/Utils/nary.c
+++ src/Utils/nary.c
@@ -29,3 +29,34 @@ TreeNode *nary_addChild(TreeNode *node, int val, bool is_leaf) {
         return addSibling(node->first_child, val, is_leaf);
     }
 }
+
+void printInorder(TreeNode *root){
+    if(!root)
+        return;
+    
+    printInorder(root->first_child);
+    printf("%d\n", root->val);
+
+    if(root->first_child){
+        TreeNode* temp = root->first_child->next_sibling;
+        while(temp){
+            printInorder(temp);
+            temp = temp->next_sibling;
+        }
+    }
+
+}
+
+int main(){
+    TreeNode* n1 = newNode(1,0);
+    nary_addChild(n1, 2, 0);
+    nary_addChild(n1, 3, 0);
+    nary_addChild(n1, 4, 1);
+    nary_addChild(n1->first_child, 5, 1);
+    nary_addChild(n1->first_child, 6, 1);
+    nary_addChild(n1->first_child->next_sibling, 7, 1);
+    nary_addChild(n1->first_child->next_sibling, 8, 1);
+
+    printInorder(n1);
+
+}
